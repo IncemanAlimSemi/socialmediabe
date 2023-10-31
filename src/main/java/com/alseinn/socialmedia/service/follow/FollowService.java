29940c137227aa712f28b066224d4df62069caf0
burcu arslan <burcuarslln@gmail.com>
2023-10-31 22:46:17 +0300

    SBDEV-2 Added Logs

--- src/main/java/com/alseinn/socialmedia/service/follow/FollowService.java
+++ src/main/java/com/alseinn/socialmedia/service/follow/FollowService.java
@@ -3,11 +3,12 @@ package com.alseinn.socialmedia.service.follow;
 import com.alseinn.socialmedia.request.follow.FollowRequest;
 import com.alseinn.socialmedia.request.follow.UnfollowRequest;
 import com.alseinn.socialmedia.response.follow.FollowResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface FollowService {
 
-    FollowResponse follow(FollowRequest followRequest);
+    FollowResponse follow(FollowRequest followRequest) throws JsonProcessingException;
 
-    FollowResponse unfollow(UnfollowRequest followRequest);
+    FollowResponse unfollow(UnfollowRequest followRequest) throws JsonProcessingException;
 
 }
--- src/main/java/com/alseinn/socialmedia/service/follow/impl/FollowServiceImpl.java
+++ src/main/java/com/alseinn/socialmedia/service/follow/impl/FollowServiceImpl.java
@@ -6,12 +6,16 @@ import com.alseinn.socialmedia.request.follow.FollowRequest;
 import com.alseinn.socialmedia.request.follow.UnfollowRequest;
 import com.alseinn.socialmedia.response.follow.FollowResponse;
 import com.alseinn.socialmedia.service.follow.FollowService;
+import com.alseinn.socialmedia.service.post.impl.PostServiceImpl;
 import com.alseinn.socialmedia.utils.UserUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
+import java.util.logging.Logger;
 
 
 @Service
@@ -20,46 +24,59 @@ public class FollowServiceImpl implements FollowService {
 
     private final UserRepository userRepository;
     private final UserUtils userUtils;
+    private final ObjectMapper mapper;
+    private static final Logger LOG = Logger.getLogger(PostServiceImpl.class.getName());
 
     @Override
-    public FollowResponse follow(FollowRequest followRequest) {
+    public FollowResponse follow(FollowRequest followRequest) throws JsonProcessingException {
         User follower = userUtils.getUserFromSecurityContext(); //takipçi
         User followed = userRepository.findByUsername(followRequest.getFollow()).orElse(null); //takip eden
-        if (checkIsSelf(follower.getUsername(), followed.getUsername())) {
-            return createFollowResponse("You can't follow yourself", false);
-        } else if (checkIsFollowExist(follower.getId(), followed.getId())) {
-            return createFollowResponse("You already follow this user", false);
-        } else if (Objects.nonNull(follower) && Objects.nonNull(followed)) {
-            try {
-                follow(follower, followed);
-                userRepository.save(follower);
-                return createFollowResponse("Followed successfully", true);
-            } catch (Exception e) {
-                return createFollowResponse("Error occurred while following", false);
+        if (Objects.nonNull(follower) && Objects.nonNull(followed)) {
+            if (checkIsSelf(follower.getUsername(), followed.getUsername())) {
+                LOG.warning("You can't follow yourself: " + mapper.writeValueAsString(followRequest));
+                return createFollowResponse("You can't follow yourself", false);
+            } else if (checkIsFollowExist(follower.getId(), followed.getId())) {
+                return createFollowResponse("You already follow this user", false);
+            } else {
+                try {
+                    follow(follower, followed);
+                    userRepository.save(follower);
+                    return createFollowResponse("Followed successfully", true);
+                } catch (Exception e) {
+                    return createFollowResponse("Error occurred while following", false);
+                }
             }
 
         }
+        LOG.warning("There is no user or the follow field is empty: " + mapper.writeValueAsString(followRequest));
         return createFollowResponse("There is no user or the follow field is empty", false);
     }
 
     @Override
-    public FollowResponse unfollow(UnfollowRequest followRequest) {
+    public FollowResponse unfollow(UnfollowRequest unfollowRequest) throws JsonProcessingException {
         User follower = userUtils.getUserFromSecurityContext(); //takipçi
-        User followed = userRepository.findByUsername(followRequest.getUnfollow()).orElse(null); //takip eden
-        if (checkIsSelf(follower.getUsername(), followed.getUsername())) {
-            return createFollowResponse("You can't unfollow yourself", false);
-        } else if (!checkIsFollowExist(follower.getId(), followed.getId())) {
-            return createFollowResponse("You don't unfollow this user", false);
-        } else if (Objects.nonNull(follower) && Objects.nonNull(followed)) {
-            try {
-                unfollow(follower, followed);
-                userRepository.save(follower);
-                return createFollowResponse("Unfollowed successfully", true);
-            } catch (Exception e) {
-                return createFollowResponse("Error occurred while unfollowing", false);
+        User followed = userRepository.findByUsername(unfollowRequest.getUnfollow()).orElse(null); //takip eden
+        if (Objects.nonNull(follower) && Objects.nonNull(followed)) {
+            if (checkIsSelf(follower.getUsername(), followed.getUsername())) {
+                LOG.warning("You can't unfollow yourself: " + mapper.writeValueAsString(unfollowRequest));
+
+                return createFollowResponse("You can't unfollow yourself", false);
+            } else if (!checkIsFollowExist(follower.getId(), followed.getId())) {
+                LOG.warning("You don't unfollow this user: " + mapper.writeValueAsString(unfollowRequest));
+
+                return createFollowResponse("You don't unfollow this user", false);
+            } else {
+                try {
+                    unfollow(follower, followed);
+                    userRepository.save(follower);
+                    return createFollowResponse("Unfollowed successfully", true);
+                } catch (Exception e) {
+                    return createFollowResponse("Error occurred while unfollowing", false);
+                }
             }
 
         }
+        LOG.warning("There is no user or the unfollow field is empty: " + mapper.writeValueAsString(unfollowRequest));
         return createFollowResponse("There is no user or the unfollow field is empty", false);
     }
 
